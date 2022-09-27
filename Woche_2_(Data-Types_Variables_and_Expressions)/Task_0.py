4661d67133125b54f01a004d8dab2a85caadb782
MasterSpam <moritz@feuchter.email>
2022-09-27 13:43:04 +0200

    Week 2 Testcode

---
+++ Woche_2_(Data-Types_Variables_and_Expressions)/Task_0.py
@@ -0,0 +1,9 @@
+# name = input("What's your name?")
+# age = input("What's your age?")
+# occupation = input("What's your occupation?")
+
+data = input("What's your name, age and occupation?")
+
+name, age, occupation = data.split()
+
+print(f"Hi {name}! I see you are {age} years old and work as a {occupation}.")
