781da5ec9cca5b4477fa999b650416b17752da08
MasterSpam <moritz@feuchter.email>
2022-09-29 11:35:37 +0200

    Testing Week 2

--- Woche_2_(Data-Types_Variables_and_Expressions)/Task_0.py
+++ Woche_2_(Data-Types_Variables_and_Expressions)/Task_0.py
@@ -7,3 +7,6 @@ data = input("What's your name, age and occupation?")
 name, age, occupation = data.split()
 
 print(f"Hi {name}! I see you are {age} years old and work as a {occupation}.")
+
+s = "abcd"
+print(s[5:6])
