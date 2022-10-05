2577c660ad2760a61d23f3bce2d18dbfa4319244
MasterSpam <moritz@feuchter.email>
2022-10-05 16:28:20 +0200

    Rename Files

---
+++ Woche_3_(Conditional_and_Iterative_Statements)/Task_3_0.py
@@ -0,0 +1,28 @@
+import time
+
+# time_start = time.time()
+
+number = 997
+result = 0
+
+# def foo(x):
+#     if x < 1:
+#         return x
+#     return x + foo(x-1)
+
+# result =foo(number)
+
+# result = sum(range(number+1))
+
+# for n in range(number + 1):
+#     result += n
+#
+#
+# time_end = time.time()
+# print(f'{time_end-time_start}: {result}')
+
+n = 5
+
+for i in range(0, n):
+    print(i)
+    n += 1
